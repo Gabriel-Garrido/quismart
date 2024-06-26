8cf74f5a3bddb2f5711479a5c60aa93d154c4a10
Gabriel Garrido <gabrielgarrido89@gmail.com>
2024-06-26 00:09:18 -0400

    install redux and typescript

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Error404 from 'containers/errors/Error404'
+import Home from 'containers/pages/Home';
+
 function App() {
   return (
-    <div className="text-gray-500 underline"> 
-      Hola mundo
-    </div>
-  )
-  
+    <Router>
+      <Routes>
+        <Route path="*" element={<Error404 />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
