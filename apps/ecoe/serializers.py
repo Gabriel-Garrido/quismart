492c35c03fdd15d6bf74829a6c3f39a57691ff35
Gabriel Garrido <gabrielgarrido89@gmail.com>
2024-06-30 13:51:43 -0400

    add puntajes in EstudianteSerializer

--- apps/ecoe/serializers.py
+++ apps/ecoe/serializers.py
@@ -1,20 +1,15 @@
 from rest_framework import serializers
 from .models import Estudiante, Evaluador, Estacion, Pregunta, Evaluacion, EvaluacionEstacion, Puntaje
 
-class EstudianteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Estudiante
-        fields = ['id', 'nombre', 'apellido', 'matricula', 'email']
-
 class EvaluadorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Evaluador
         fields = ['id', 'nombre', 'apellido', 'especialidad', 'email']
-        
+
 class PreguntaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pregunta
-        fields = ['id', 'texto', 'estacion']
+        fields = ['id', 'texto']
 
 class EstacionSerializer(serializers.ModelSerializer):
     evaluador = EvaluadorSerializer(read_only=True)
@@ -33,11 +28,26 @@ class EvaluacionEstacionSerializer(serializers.ModelSerializer):
 
 class EvaluacionSerializer(serializers.ModelSerializer):
     estaciones = EvaluacionEstacionSerializer(source='evaluacionestacion_set', many=True, read_only=True)
-    estudiantes = EstudianteSerializer(many=True, read_only=True)
+    estudiantes = serializers.StringRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Evaluacion
         fields = ['id', 'fecha', 'duracion', 'tipo', 'estaciones', 'estudiantes']
+
+class PuntajeDetailSerializer(serializers.ModelSerializer):
+    evaluacion = EvaluacionSerializer(read_only=True)
+    estacion = EstacionSerializer(read_only=True)
+
+    class Meta:
+        model = Puntaje
+        fields = ['id', 'evaluacion', 'estacion', 'puntaje', 'comentario', 'feedback']
+
+class EstudianteSerializer(serializers.ModelSerializer):
+    puntajes = PuntajeDetailSerializer(source='puntaje_set', many=True, read_only=True)
+
+    class Meta:
+        model = Estudiante
+        fields = ['id', 'nombre', 'apellido', 'matricula', 'email', 'puntajes']
 
 class PuntajeSerializer(serializers.ModelSerializer):
     estudiante = EstudianteSerializer(read_only=True)
